deab7c1b1ef2486d5ac11e0c39773612244ff722
Brandon Richards <brandonrichardsaz@gmail.com>
2017-04-12 16:32:56 -0700

    Created Initial Program

---
+++ Swapping Screens/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Swapping Screens
+//
+//  Created by Richards, Brandon S. on 4/12/17.
+//  Copyright © 2017 Richards, Brandon S. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+
+
+}
+
